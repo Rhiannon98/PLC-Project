c8d60077148ae0c509eae3045e1436112ba382f8
Anthony Green <Anthonydgreen90@gmail.com>
2018-03-26 13:56:43 -0700

    added game actions and game ending function

--- js/game.js
+++ js/game.js
@@ -1,26 +1,30 @@
 'use strict';
-var endPoint = 100;
+var endPoint = 10;
+
+var optionArray = [];
 
 //TODO: pull from local storage (test name is ted)
 var playerName = 'ted';
 
 var optionsElement = document.getElementById('options');
 
-function Player(name, time, money, energy){
+function Player(name, time, money, energy, distanceTravelled = 0){
   this.name = name;
   this.time = time;
   this.money = money;
   this.energy = energy;
+  this.distanceTravelled = distanceTravelled;
 }
 
 var testPlayer = new Player(playerName, 10, 10, 10);
 
-function Option(name, time, money, energy) {
-
+function Option(name, time, money, energy, distance) {
   this.name = name;
   this.time = time;
   this.money = money;
   this.energy = energy;
+  this.distance = distance;
+  optionArray.push(this);
 }
 
 var testOption1 = new Option('test', 5, 5, 5);
@@ -32,16 +36,29 @@ function optionClick(event) {
   var clickedOption = event.target.id;
 
   if(clickedOption === 'option1') {
-    console.log('option 1');
+    testPlayer.money--;
+    testPlayer.distanceTravelled +=3;
+    console.log(testPlayer);
   } else if
   (clickedOption === 'option2') {
-    console.log('option 2');
+    testPlayer.time--;
+    testPlayer.distanceTravelled +=2;
+    console.log(testPlayer);
   }
   else if
   (clickedOption === 'option3'){
-    console.log('option 3');
+    testPlayer.energy++;
+    testPlayer.distanceTravelled ++;
+    console.log(testPlayer);
+  }
+  if (testPlayer.distanceTravelled >= endPoint){
+    //this is where the game ends
+    optionsElement.removeEventListener('click', optionClick);
+    console.log('game end');
   }
 }
+
+
 
 
 optionsElement.addEventListener('click', optionClick);
