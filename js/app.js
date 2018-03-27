1d48431db6065eca2f112957e16391babad6e244
Rhiannon Mortensen <34196086+Rhiannon98@users.noreply.github.com>
2018-03-26 17:33:12 -0700

    Revert "Mon rhi"

--- js/app.js
+++ js/app.js
@@ -1,21 +1,10 @@
 'use strict';
 
-// grabbing the element from the dom
 var userNameElement = document.getElementById('user');
 
-// creating a validation prompt upon submit
-// naming it validation to edit this later
-function validateInput(event) {
-  //self explanatory
+function validateInput(event){
   event.preventDefault();
-
-  var userName = event.target.name.value;
-
-  //proof of life
-  alert('hello ' + userName + '!');
-  localStorage.setItem('validateInput', JSON.stringify(userName));
+  console.log(event.target.name.value);
 }
 
-// event listener for above event function
-userNameElement.addEventListener('submit', validateInput);
-
+userNameElement.addEventListener('click', validateInput);
