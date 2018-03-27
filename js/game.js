f60dad8381a389849f367ae04e636c89145d2fc5
Rhiannon98 <mortensenrhi@gmail.com>
2018-03-26 17:21:26 -0700

    added comments. and element link

--- js/game.js
+++ js/game.js
@@ -18,7 +18,14 @@ var optionsElement = document.getElementById('options');
 var progress = localStorage.getItem('playerArray');
 playerArray = JSON.parse(progress);
 
+// elements from the dom
+// to be used to display the different options.
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
 
+
+// to go through local storage to see if name is a previous player
 function checkName() {
   for (var n in playerArray) {
     if (playerName === n.name) {
@@ -29,7 +36,8 @@ function checkName() {
 }
 
 
-
+// new player constructor
+// TODO: get userName from app.js form
 function Player(name, time, money, energy, distanceTravelled = 0) {
   this.name = name;
   this.time = time;
@@ -39,7 +47,8 @@ function Player(name, time, money, energy, distanceTravelled = 0) {
   playerArray.push(this);
 }
 
-
+// option constructor aids in making options for player to select from
+// each option has either rewards or consequences for parameters
 function Option(name, time, money, energy, distance) {
   this.name = name;
   this.time = time;
@@ -49,10 +58,12 @@ function Option(name, time, money, energy, distance) {
   optionArray.push(this);
 }
 
+// new option indices
 var testOption1 = new Option('test', 5, 5, 5);
 var testOption2 = new Option('test2', 3, 3, 3);
 var testOption3 = new Option('test3', 1, 1, 1);
 
+// click event for when user selects one of the above options
 function optionClick(event) {
 
   var clickedOption = event.target.id;
@@ -87,9 +98,10 @@ function optionClick(event) {
 
 
 
-
+// 'calling' on the click event when user inputs
 optionsElement.addEventListener('click', optionClick);
 
+// calling on the checkName function on page load
 checkName();
 
 
