cc848134d98da0498104e1756b2b671f69105599
Alexander Stone <aloecend@gmail.com>
2018-03-27 15:27:03 -0700

    made findNode function when loops occur in the graph.

--- js/graph.js
+++ js/graph.js
@@ -4,22 +4,37 @@ function Tree(head = null){
   this.head = head;
 }
 //This function will search the Tree for the Parent, then add a node to its links. If the parent is not in the tree, it does nothing
-function findNode(targetName, start){
+//visited is always empty and never given a value by the user, only the function
+function findNode(targetName, start, visited = []){
   //if this is the right node, return it
   if (start.name === targetName){
     return start;
   }
   //If there are no more places to visit, return false
   else if (start.links === []){
+    visited.push(start);
     return false;
   }
   //otherwise recursively search each link, stopping at the first successful find
   else {
+    //if we've been here before, return false because we already know this is a dead end
+    for (let n in visited){
+      if (start === visited[n]){
+        return false;
+      }
+    }
+    visited.push(start);
     for (let i in start.links){
-      var result = findNode(targetName, start.links[i]);
-      //if this branch has what we are looking for, return true
-      if (result !== false){
-        return result;
+      //if i is null, remove it from the links
+      if (i === null){
+        start.links.splice(i, 1);
+      }
+      else {
+        var result = findNode(targetName, start.links[i]);
+        //if this branch has what we are looking for, return true
+        if (result !== false){
+          return result;
+        }
       }
     }
     return false;
@@ -46,17 +61,15 @@ function linkNodes(linkername, linkeename, start){
   }
   return false;
 }
-//zombiecode for removeNode function
-// function removeNode(targetName, start){
-//   nodeToRemove = findNode(targetName, start);
-//   if (nodeToRemove === false){
-//     return false;
-//   }
-//   else {
-//    delete nodeToRemove
-//    //if we ever need to use this, findNode will need to be able to handle Null
-//   }
-//}
+function removeNode(targetName, start){
+  var nodeToRemove = findNode(targetName, start);
+  if (nodeToRemove === false){
+    return false;
+  }
+  else {
+    nodeToRemove = null;
+  }
+}
 
 //Data will be whatever object the node is storing and links is an array of Nodes this can vist
 function Node( name, links = []){
