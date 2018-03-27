c0d88f2a07420ed51cf9d46ab66101ad71cd1800
Rhiannon98 <mortensenrhi@gmail.com>
2018-03-27 11:46:26 -0700

    mob session with alex and timea

--- js/game.js
+++ js/game.js
@@ -1,4 +1,7 @@
 'use strict';
+
+// TODO: create current user variable to access later
+
 var endPoint = 20;
 
 //holding the various options
@@ -6,24 +9,12 @@ var optionArray = [];
 //holding the varius player
 var playerArray = [];
 
+// getting the element from the dom
 var optionsElement = document.getElementById('options');
 
-var progress = localStorage.getItem('playerArray');
-playerArray = JSON.parse(progress);
 
-
-function checkName() {
-  for(var n in playerArray){
-    if(playerName === n.name){
-      var player = n;
-      return player;
-    }
-  }
-}
-
-
-
-function Player(name, time, money, energy, distanceTravelled = 0){
+// creating constructor function for new player instances
+function Player(name, time, money, energy, distanceTravelled = 0) {
   this.name = name;
   this.time = time;
   this.money = money;
@@ -33,6 +24,7 @@ function Player(name, time, money, energy, distanceTravelled = 0){
 }
 
 
+// to create new questions (options)
 function Option(name, time, money, energy, distance) {
   this.name = name;
   this.time = time;
@@ -42,48 +34,92 @@ function Option(name, time, money, energy, distance) {
   optionArray.push(this);
 }
 
+// pull the userName from validateInput in app.js
+function getUser() {
+  var newUser = localStorage.getItem('validateInput');
+
+  newUser = JSON.parse(newUser);
+  // TODO:
+  // FIXME: numbers are filler for now.
+
+  var progress = localStorage.getItem('playerArray');
+  if (progress === null) {
+    var newPlayer = new Player(newUser, 10, 10, 10);
+  } else {
+    playerArray = JSON.parse(progress);
+    var checkUser = checkName(newUser);
+    if (checkUser === false) {
+      newPlayer = new Player(newUser, 10, 10, 10);
+    }
+  }
+}
+
+// to go through local storage to see if name is a previous player
+function checkName(playerName) {
+  for (var n in playerArray) {
+    if (playerName === n.name) {
+      var player = n;
+      return player;
+    }
+  }
+  return false;
+}
+
+// save playerArray into LS
+function saveProgress() {
+  // storing player array in local storage.
+  localStorage.setItem('playerArray', JSON.stringify(playerArray));
+}
+
 var testOption1 = new Option('test', 5, 5, 5);
-var testOption2 = new Option('test2', 3,3,3);
+var testOption2 = new Option('test2', 3, 3, 3);
 var testOption3 = new Option('test3', 1, 1, 1);
 
+// creating click event function
 function optionClick(event) {
 
+  // button user clicked on
   var clickedOption = event.target.id;
 
-  if(clickedOption === 'option1') {
-    player.money -= 10;
-    player.distanceTravelled +=3;
-    console.log(player);
+  // change the players resources based on the option that they clicked on
+  if (clickedOption === 'option1') {
+    Player.money -= 5;
+    Player.distanceTravelled += 3;
+    console.log(Player);
   } else if
   (clickedOption === 'option2') {
-    player.time-= 10;
-    player.distanceTravelled +=2;
-    console.log(player);
+    Player.time -= 5;
+    Player.distanceTravelled += 2;
+    console.log(Player);
   }
   else if
-  (clickedOption === 'option3'){
-    player.energy -= 10;
-    player.distanceTravelled ++;
-    console.log(player);
+  (clickedOption === 'option3') {
+    Player.energy -= 5;
+    Player.distanceTravelled++;
+    console.log(Player);
   }
-  if (player.distanceTravelled >= endPoint){
+  else if (clickedOption === 'saveProgress') {
+    saveProgress();
+  }
+  // if user at end, they win
+  if (Player.distanceTravelled >= endPoint) {
     //this is where the game ends
     optionsElement.removeEventListener('click', optionClick);
     console.log('game won');
   }
-  else if ((player.money <= 0) || (player.energy <= 0) || (player.time <= 0)){
+  // if player has 0 of any resource = LOSER
+  else if ((Player.money <= 0) || (Player.energy <= 0) || (Player.time <= 0)) {
     optionsElement.removeEventListener('click', optionClick);
     console.log('game lost');
   }
-  localStorage.setItem('playerArray', JSON.stringify(playerArray));
+
 }
-
-
 
 
 optionsElement.addEventListener('click', optionClick);
 
-checkName();
+getUser();
+// checkName();
 
 
 
