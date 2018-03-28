7e6095ec85fad7aa60342eae5dccd5fdb2f40add
Rhiannon98 <mortensenrhi@gmail.com>
2018-03-28 16:33:02 -0700

    got leaderArray to show in console

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,22 +1,18 @@
 'use strict';
 
-//to store the leaders not obverwrite
+//TODO: store the leaders not obverwrite
 var leaderArray = [];
 //this gets the current player out of local storage
 var currentPlayer = JSON.parse(localStorage.getItem('currentPlayer'));
+var playersArray = JSON.parse(localStorage.getItem('playerArray'));
 //
 var finalScore = currentPlayer.energy + currentPlayer.time + currentPlayer.money;
 //get list element from DOM
-var leaderList = document.getElementById('leaders');
-leaderList.textContent = ('Here are the best players');
-console.log('currentplayer', currentPlayer);
+var topPlayers = document.getElementById('leaders');
+topPlayers.textContent = ('Here are the best players');
+console.log('currentplayer = ', currentPlayer);
 
-//creates it's own <li>
-var topPlayers = document.createElement('li');
-topPlayers.textContent = (currentPlayer.name + ' ' + finalScore);
-leaderList.appendChild(topPlayers);
 //pushes top players into leaders array to be stored and displayed on the board
-
 function LeaderBoard(name, score) {
   this.name = name;
   this.score = score;
@@ -25,15 +21,23 @@ function LeaderBoard(name, score) {
 
 console.log(leaderArray);
 
-new LeaderBoard(currentPlayer.name, finalScore);
+// new LeaderBoard(currentPlayer.name, finalScore);
 
 
 //function to sort through the playerArray by points for leaderboard
-function compareInArray( a, b ){
-  //TODO: Add a points system or change .points to be something else
-  return b.points - a.points;
+function compareInArray(a, b) {
+  // sorts the scores in descending order
+  return b.score - a.score;
 }
 //sorts array by player scores
-playerArray.sort(compareInArray);
+leaderArray.sort(compareInArray);
 
 //TODO: display sorted list on DOM
+function displayLeaderboard() {
+
+  for (var i in playersArray) {
+    new LeaderBoard(playersArray[i].name, finalScore);
+  }
+}
+
+displayLeaderboard();
