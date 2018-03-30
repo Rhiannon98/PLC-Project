ddeaa44689f31d49f5497918253b0dbee388a6b8
Rhiannon98 <mortensenrhi@gmail.com>
2018-03-30 09:12:38 -0700

    made it a bit easier to win

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@
 // TODO: get this working ... better
 
 
-var endPoint = 60;
+var endPoint = 45;
 
 
 //holding the various options
@@ -84,24 +84,24 @@ new Question('Whomp Whomp! A $8 billion dollar tunnel-digging machine has broked
 new Question('That was weird! Your transit route went exactly as expected. How long will that last?', 'energy', -1);
 new Question('Where is all the traffic? Does everyone else have a holiday except for you? (Time +5)', 'time', -5);
 new Question('Not again! Somehow a third avenue crazy has interupted your commute. You try to politely say that you do not have spare money but get bombarded with insults. (Energy -5)', 'energy', 5);
-new Question('You pass some tents, they are really close to the light intersection that you are at. Suddenly, a man pops out of the tent and begins dancing and prancing around with a large stick in the middle of the intersection. The police must get involved, you get interviewed about the incident for being a witness. You lose time.','time', 4);
+new Question('You pass some tents, they are really close to the light intersection that you are at. Suddenly, a man pops out of the tent and begins dancing and prancing around with a large stick in the middle of the intersection. The police must get involved, you get interviewed about the incident for being a witness. You lose time.', 'time', 4);
 new Question('Suddenly the clouds that have been hovering the entire day open up and let loose their tears. The rain comes pouring down in waves, soaking everything through, even those people inside buildings. Everyone begins to shiver with the cold. You pull your jacket closer to you and press on. Just as quickly as it appears it leaves. Classic Seattle.', 'energy', 3);
-new Question ('It starts as a little white coating that gets added to everything, just a tiny bit of snow. But soon enough it comes down in a giant, swirling storm. The blizzard creates a wall of ice that stops all transit for a little bit. However you are nothing if not resourceful and are able to scale the wall, but it takes a while.', 'time', 4);
-new Question (' Quite suddenly an owl lands on you with a small letter attached to its leg. You take the letter out and begin to read it. It is an acceptance letter for Hogwarts. You debate to yourself the merits of dropping this whole web developer thing and becoming a wizard, but eventually decide that while being a wizard would be very cool, it would not pay the bills. You resume your journey a short time later.', 'time', 1);
-new Question ('A cat brushes your leg, so you move down to begin petting it. You notice that it is carrying a five dollar bill in its mouth. You attempt to remove the dollar from its mouth, but the cat will have none of it. You stare it down and prepare for the inevitable duel to the death. As you both circle each other, waiting for an opportunity to strike, a five dollar bill falls from the sky. You pick it up and you and the cat go on your way, death feud forgotten.', 'money', -5);
-new Question ('Disturbingly, transit is working and on time. Nothing goes wrong and nothing unusual happens. You are fairly certain you have entered an alternate universe somehow.', 'time', 0);
-new Question ('A section of the city has been closed off by police for reason they refuse to discuss. You have to take a large detour around them. When you finish the detour you notice that they have moved downward and you need to take another detour. Finally you get ahead of the police who seem to be methodically searching this stree for something or someone.', 'time', 2);
-new Question('A bank robbery is occuring, but the robbers are short handed. They offer you a portion of the take if you help them out. You don your ridiculous face mask and take the fire extinguisher they give you, then walk into the bank. Several confusing minutes later you are getting out of a getaway van at your next stop, your pockets considerably heavier. +25 Money', 'money', -25);
-new Question ('You are on your usual commute, suddenly you here screaming coming from behind you. The man is just screaming \'CHOCOLATE\' as loudly as he can as he gets closer to you. He grabs you by your shirt collar and lets another scream reverberate your brain. He calmly put you down, hands you money and steals your chocolate bar. You are unsure about this situation....  the distraction delayed your commute.', 'time', 1);
-new Question ('Someone got offended, planet Earth stopped its orbit, and now everyone is dead. The planet gets to start over, but you lose.', 'energy', 100);
-new Question ('A pet pig ran into the street and totalled a Prius. Everyone stops to pet the pig, beacuse it\'s got and ouchy. You win 10 energy points, because one less Prius.', 'energy', -10);
-new Question ('A siren begins to blare, as usual in a over-populated city. You and everyone else stops to allow them to pass, you are minorly inconvenienced.', 'time', 1);
-new Question ('Miss Peregrine invited you in her time-loop, now you stuck in infinite commute.', 'time', 100);
-new Question ('You feel a grumbling in your stomach, and a slight discomfort. You hope your commute goes faster so that you are able to get to your destination to use the clean bathrooms there as opposed to anything that is public.', 'energy', 1);
-new Question ('You trip and faceplant the asphalt, your pride is hurt and you had most certainly slowed down. But! You have found a $10 bill, it almost gets you a hand-crated gluten free tulipmilk-latte.', 'money', -10);
-new Question ('There is a warning sign that you or anyone else have ever seen, judging from other bystanders faces. The sign is like an animal crossing sign, except you do not recognize the animals pictured. Suddenly, you understand as a hoard of tourists on segways goes by you. Oh lord, it is like an entire office building of people going by, you feel yourself aging and becoming more impatient as the hundreds of people on slow segways blow by.', 'time', 3);
-new Question ('It is really early today and as you pass a misty field, you see a gentleman shuffling towards you. On closer inspection OMFG! IT\'S THE ZOMBIE APOCALIPSE! You enter warp speed, and get to the school ahead of time. You Win! (At least for now, go get yourself a cat \'cause you know OMFG! IT\'S THE ZOMBIE APOCALIPSE!', 'distanceTravelled', -100);
-new Question ('You become lost in thought and are suddenly at your destination. Wait... no, this is the space needle. WHY ARE YOU HERE?!?! HOW DID YOU COMPLETELY MESS UP YOUR ROUTE LIKE THIS?!?! *sigh* you have to backtrack now. Bummer. -15 Distance To Destination.', 'distanceTravelled', -15);
+new Question('It starts as a little white coating that gets added to everything, just a tiny bit of snow. But soon enough it comes down in a giant, swirling storm. The blizzard creates a wall of ice that stops all transit for a little bit. However you are nothing if not resourceful and are able to scale the wall, but it takes a while.', 'time', 4);
+new Question(' Quite suddenly an owl lands on you with a small letter attached to its leg. You take the letter out and begin to read it. It is an acceptance letter for Hogwarts. You debate to yourself the merits of dropping this whole web developer thing and becoming a wizard, but eventually decide that while being a wizard would be very cool, it would not pay the bills. You resume your journey a short time later.', 'time', 1);
+new Question('A cat brushes your leg, so you move down to begin petting it. You notice that it is carrying a five dollar bill in its mouth. You attempt to remove the dollar from its mouth, but the cat will have none of it. You stare it down and prepare for the inevitable duel to the death. As you both circle each other, waiting for an opportunity to strike, a five dollar bill falls from the sky. You pick it up and you and the cat go on your way, death feud forgotten.', 'money', -5);
+new Question('Disturbingly, transit is working and on time. Nothing goes wrong and nothing unusual happens. You are fairly certain you have entered an alternate universe somehow.', 'time', 0);
+new Question('A section of the city has been closed off by police for reason they refuse to discuss. You have to take a large detour around them. When you finish the detour you notice that they have moved downward and you need to take another detour. Finally you get ahead of the police who seem to be methodically searching this stree for something or someone.', 'time', 2);
+new Question('A bank robbery is occuring, but the robbers are short handed. They offer you a portion of the take if you help them out. You don your ridiculous face mask and take the fire extinguisher they give you, then walk into the bank. Several confusing minutes later you are getting out of a getaway van at your next stop, your pockets considerably heavier. +40 Money', 'money', -40);
+new Question('You are on your usual commute, suddenly you here screaming coming from behind you. The man is just screaming \'CHOCOLATE\' as loudly as he can as he gets closer to you. He grabs you by your shirt collar and lets another scream reverberate your brain. He calmly put you down, hands you money and steals your chocolate bar. You are unsure about this situation....  the distraction delayed your commute.', 'time', 1);
+new Question('Someone got offended, planet Earth stopped its orbit, and now everyone is dead. The planet gets to start over, but you lose.', 'energy', 100);
+new Question('A pet pig ran into the street and totalled a Prius. Everyone stops to pet the pig, beacuse it\'s got and ouchy. You win 10 energy points, because one less Prius.', 'energy', -10);
+new Question('A siren begins to blare, as usual in a over-populated city. You and everyone else stops to allow them to pass, you are minorly inconvenienced.', 'time', 1);
+new Question('Miss Peregrine invited you in her time-loop, now you stuck in infinite commute.', 'time', 100);
+new Question('You feel a grumbling in your stomach, and a slight discomfort. You hope your commute goes faster so that you are able to get to your destination to use the clean bathrooms there as opposed to anything that is public.', 'energy', 1);
+new Question('You trip and faceplant the asphalt, your pride is hurt and you had most certainly slowed down. But! You have found a $10 bill, it almost gets you a hand-crated gluten free tulipmilk-latte.', 'money', -10);
+new Question('There is a warning sign that you or anyone else have ever seen, judging from other bystanders faces. The sign is like an animal crossing sign, except you do not recognize the animals pictured. Suddenly, you understand as a hoard of tourists on segways goes by you. Oh lord, it is like an entire office building of people going by, you feel yourself aging and becoming more impatient as the hundreds of people on slow segways blow by.', 'time', 3);
+new Question('It is really early today and as you pass a misty field, you see a gentleman shuffling towards you. On closer inspection OMFG! IT\'S THE ZOMBIE APOCALIPSE! You enter warp speed, and get to the school ahead of time. You Win! (At least for now, go get yourself a cat \'cause you know OMFG! IT\'S THE ZOMBIE APOCALIPSE!', 'distanceTravelled', -100);
+new Question('You become lost in thought and are suddenly at your destination. Wait... no, this is the space needle. WHY ARE YOU HERE?!?! HOW DID YOU COMPLETELY MESS UP YOUR ROUTE LIKE THIS?!?! *sigh* you have to backtrack now. Bummer. -15 Distance To Destination.', 'distanceTravelled', -15);
 
 // pull the userName from validateInput in app.js
 // confirm that this user is or is not the same as one in the playerArray
@@ -116,14 +116,14 @@ function getUser() {
   var progress = localStorage.getItem('playerArray');
   if (progress === null) {
     //if playerArray DNE, make user
-    new Player(newUser, 25, 25, 25);
+    new Player(newUser, 40, 40, 40);
 
   } else {
     playerArray = JSON.parse(progress);
     var checkUser = checkName(newUser);
     if (checkUser === false) {
       //if player DNE, make player
-      new Player(newUser, 25, 25, 25);
+      new Player(newUser, 40, 40, 40);
     }
   }
 }
@@ -191,7 +191,7 @@ function renderProgressBar() {
     // TODO: get good colours
 
     // rest of the ifs and else ifs are assigning the color for the bar at that % it os filled
-    if (currentPlayer.distanceTravelled / endPoint < 0.25) {
+    if (currentPlayer.distanceTravelled / endPoint < 0.40) {
       color = '#ff0000';
     }
     else if (currentPlayer.distanceTravelled / endPoint < 0.5) {
@@ -240,7 +240,7 @@ function optionClick(event) {
 
   newQuestion.loadText();
   //computing differences in fantastic if else blocks
-  if (newQuestion.resource === 'distanceTravelled'){
+  if (newQuestion.resource === 'distanceTravelled') {
     currentPlayer.resources += newQuestion.amount;
   }
   else if (newQuestion.resource === 'energy') {
@@ -295,12 +295,12 @@ function optionClick(event) {
     location.href = 'leaderboard.html';
     return;
   }
-  else if (currentPlayer.energy <= 0){
+  else if (currentPlayer.energy <= 0) {
     optionsElement.removeEventListener('click', optionClick);
     alert('I get it, really. Go take a nap and try again another day. You\'re out of energy. You lose.');
     location.href = 'leaderboard.html';
     return;
-  } else if (currentPlayer.time <= 0){
+  } else if (currentPlayer.time <= 0) {
     optionsElement.removeEventListener('click', optionClick);
     alert('HOW EMBARRASSING! You are way too late. You ran out of time. You lose.');
     location.href = 'leaderboard.html';
